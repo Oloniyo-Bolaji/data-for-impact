2a2bf47dd1ad8d40bc7a59b44438a957cefbde21
unknown <user.name>
2025-09-15 22:10:01 +0100

    Performance improved

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,10 +1,8 @@
 import Header from "@/component/Header";
 import React from "react";
-import dynamic from "next/dynamic";
-
-const About = dynamic(() => import("@/component/About"));
-const FounderNote = dynamic(() => import("@/component/FounderNote"));
-const Mentor = dynamic(() => import("@/component/Mentor"));
+import About from "@/component/About";
+import FounderNote from "@/component/FounderNote";
+import MentorCard from "@/dynamic-component/MentorCardWrapper";
 
 const page = () => {
   return (
@@ -13,7 +11,7 @@ const page = () => {
       <div className="max-w-screen-xl mx-auto lg:px-10 sm:px-10 px-5 my-2">
         <About />
         <FounderNote />
-        <Mentor />
+        <MentorCard />
       </div>
     </main>
   );
